f15d0a3c0b8ac59a73c27744fb84c0b0c0adc34c
matias oggero <matiassoggero@gmail.com>
2024-03-06 01:58:43 -0300

    add category

--- dashboard-altaPinta/src/components/ContentRowTop.jsx
+++ dashboard-altaPinta/src/components/ContentRowTop.jsx
@@ -1,8 +1,10 @@
+import CategoryDb from "./CategoryDb"
+
 function ContentRowTop() {
     return (
         <div className="container-fluid">
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 className="h3 mb-0 text-gray-800" style={{"margin-top": "15px"}}>Alta Pinta Dashboard</h1>
+                <h1 className="h3 mb-0 text-gray-800" >Alta Pinta Dashboard</h1>
             </div>
             <div className="row">
 
@@ -13,6 +15,10 @@ function ContentRowTop() {
                                 <div className="col mr-2">
                                     <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Total de Productos</div>
                                     <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
+
+                                    <div className="col-auto">
+                                        <i className="fas fa-hamburger fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -26,15 +32,18 @@ function ContentRowTop() {
                                 <div className="col mr-2">
                                     <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total de Usuarios</div>
                                     <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
+                                    <div className="col-auto">
+                                        <i className="fas fa-users fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
-                              
+
                             </div>
                         </div>
                     </div>
                 </div>
 
             </div>
-         
+
             <div className="row">
                 <div className="col-lg-6 mb-4">
                     <div className="card shadow mb-4">
@@ -42,83 +51,19 @@ function ContentRowTop() {
                             <h5 className="m-0 font-weight-bold text-gray-800">Ultimo ingrediente agregado</h5>
                         </div>
                         <div className="card-body">
-                          
-                            <img className="w-100" src="/images/zanahorias-confi.jpg" alt="Toldo" style={{ maxWidth: '200px' }}/>
+
+                            <img className="w-100" src="/images/zanahorias-confi.jpg" alt="Toldo" style={{ maxWidth: '200px' }} />
                             <p>"Cuidadosamente cocinadas hasta lograr un caramelizado dorado, estas zanahorias ofrecen un sabor indulgente y una textura tierna"</p>
                             <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver más detalle</a>
                         </div>
                     </div>
                 </div>
-      
-                <div className="col-lg-6 mb-4">
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h5 className="m-0 font-weight-bold text-gray-800">Total de Categorías</h5>
-                        </div>
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Hamburguesa
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Panes
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Aderezos
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Fiambres
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Quesos
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Hojas Verdes
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Verduras Crudas
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mb-4">
-                                    <div className="card bg-dark text-white shadow">
-                                        <div className="card-body">
-                                            Verduras Cocidas
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                <CategoryDb />
+
             </div>
         </div>
+
     )
 }
 
